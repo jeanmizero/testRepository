9f59358afc67423141e523d534a0dbe6d597907e
Jean <mizero2@gmail.com>
2020-11-17 11:58:43 -0700

    add some HTML and javaScript code

---
+++ index.js
@@ -0,0 +1,16 @@
+// initialize variables
+var bankAccountBalance = 52.10;
+var firstName = "Jean";
+var lastName = "Mizero";
+var numberOfFriends = 5;
+var isHotOutside = false;
+var isHappy = true;
+console.log("Hello world");
+console.log(5 + 3);
+console.log(4 - 3);
+console.log(5 * 3);
+console.log(9 / 3);
+console.log(bankAccountBalance - 30);
+console.log(bankAccountBalance * numberOfFriends );
+console.log(5 == 5);
+console.log(5 == 6);
